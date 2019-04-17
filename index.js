53a3428489e60b9fa3f9241a42ac8f7185355819
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-17 16:43:04 +0100

    Switched to vanilla postgres from the sequelize orm

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ import { config } from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import debug from 'debug';
+import bodyParser from 'body-parser';
+
+import noteRouter from './routers/noteRouter';
 
 config();
 const devLogger = debug('ENTRY FILE');
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(noteRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: 'Incorrect url'
+  });
+});
 
 app.listen('3000', () => {
   devLogger('Note+ started on port 3000');
