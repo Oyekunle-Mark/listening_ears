5ef2a761d4cbb9db616549eb65656e1e4a6bd1be
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-18 11:50:53 +0100

    Include the edit note route

--- router/noteRouter.js
+++ router/noteRouter.js
@@ -7,5 +7,6 @@ const noteRouter = express.Router();
 noteRouter.post('/note', NoteController.createNote);
 noteRouter.get('/note', NoteController.getNote);
 noteRouter.delete('/note/:id', NoteController.deleteNote);
+noteRouter.patch('/note/:id', NoteController.editNote);
 
 export default noteRouter;
