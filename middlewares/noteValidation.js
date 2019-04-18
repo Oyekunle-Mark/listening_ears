1ee1e99fab0b2fbf9e20e3d097fafa361d9b5db2
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-18 13:08:09 +0100

    Validation for the endpoints

---
+++ middlewares/noteValidation.js
@@ -0,0 +1,43 @@
+class NoteValidation {
+  static createNoteValidation(req, res, next) {
+    if (!req.body.title || !req.body.note || /^\s*$/.test(req.body.title) || /^\s*$/.test(req.body.note)) {
+      res.status(400).json({
+        status: 400,
+        message: 'Please provide a title and note body'
+      });
+    }
+
+    next();
+  }
+
+  static deleteNote(req, res, next) {
+    if (!(/^[123456789]+$/.test(req.params.id))) {
+      res.status(400).json({
+        status: 400,
+        message: 'Request parameter must be an integer'
+      });
+    }
+
+    next();
+  }
+
+  static editNoteValidation(req, res, next) {
+    if (!req.body.title || !req.body.note || /^\s*$/.test(req.body.title) || /^\s*$/.test(req.body.note)) {
+      res.status(400).json({
+        status: 400,
+        message: 'Please provide a title and note body'
+      });
+    }
+
+    if (!(/^[123456789]+$/.test(req.params.id))) {
+      res.status(400).json({
+        status: 400,
+        message: 'Request parameter must be an integer'
+      });
+    }
+
+    next();
+  }
+}
+
+export default NoteValidation;
