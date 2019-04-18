c92a06fd7f674e02896c3ae929c15562cc36d077
oyekunle-mark <oyekunlemac@gmail.com>
2019-04-18 13:10:24 +0100

    added the validation middlewares to the routes

--- router/noteRouter.js
+++ router/noteRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 import NoteController from '../models/noteQueries';
+import NoteValidation from '../middlewares/noteValidation';
 
 const noteRouter = express.Router();
 
-noteRouter.post('/note', NoteController.createNote);
+noteRouter.post('/note', NoteValidation.createNoteValidation, NoteController.createNote);
 noteRouter.get('/note', NoteController.getNote);
-noteRouter.delete('/note/:id', NoteController.deleteNote);
-noteRouter.patch('/note/:id', NoteController.editNote);
+noteRouter.delete('/note/:id', NoteValidation.deleteNote, NoteController.deleteNote);
+noteRouter.patch('/note/:id', NoteValidation.editNoteValidation, NoteController.editNote);
 
 export default noteRouter;
